59d73ca4d0d9a3c7e27c2978ccf7c4b7687bb28f
Dzmitry D <elzord745@gmail.com>
2024-02-16 14:36:26 +0100

    Zadanie do domu

---
+++ Homologacja/Controllers/LogowanieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homologacja.Controllers
+{
+    public class LogowanieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Remember_Password()
+        {
+            return View();
+        }
+    }
+}
---
+++ Homologacja/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homologacja.Models
+{
+    public class Login
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+    }
+}
